1ae1b2eebf81c5f697429494ba0c8607d51aba7b
Vollells <vicle728@student.liu.se>
2022-01-25 20:56:43 +0100

    Fixed a simpler, still non working, version.

--- labs/intervalcover.cc
+++ labs/intervalcover.cc
@@ -2,84 +2,100 @@
 #include <vector>
 #include <algorithm>
 
-struct Interval{
-    int index;
-    float start;
-    float end;
-    bool operator < (const Interval& i) const { return (end < i.end); }
+struct interval{
+  int index;
+  float start;
+  float end;
+  bool operator < (const interval& i) const { return (start < i.start); }
 };
 
-std::vector<int> cover(Interval intervalTC, std::vector<Interval> *intervals){
-    for (auto a : *intervals){
-        std::cout << "Start: " << a.start << "end "<< a.end << std::endl;
-    }
-    std::sort(intervals->begin(), intervals->end());
-    for (auto a : *intervals){
-        std::cout << "Start: " << a.start << "end "<< a.end << std::endl;
+/* Test case with every case i can imagine. (Have tested with big ints and decimals).
+0 10
+12
+-3 -2
+-2.5 1
+0 5
+0 6
+4 7
+4 7
+6 10
+6 10
+9.5 14
+13 14
+13 14
+22 23
+2
+3 6
+*/
+
+std::vector<int> cover(interval interval_goal, std::vector<interval> *intervals){
+
+  std::sort(intervals->begin(), intervals->end());
+  std::vector<int> cover;
+
+  interval covered = {-1, interval_goal.start, interval_goal.start };
+  int indexTF = 0;
+  for (interval inter : *intervals){
+    /*
+      Case 1: If same pos/earlier and better!
+      Case 2: If after the previous start add as new refrence!
+    */
+    if (inter.start <= covered.start && ((cover.empty() && inter.end >= covered.end)||
+                                         (!cover.empty() && inter.end > covered.end))){
+      if (!cover.empty()){
+        cover.pop_back();
+      }
+      covered.end = inter.end;
+      cover.push_back(intervals->at(indexTF).index);
+    } else if (inter.start <= covered.end && inter.end > covered.end){
+      covered.start = covered.end;
+      covered.end = inter.end;
+      cover.push_back(intervals->at(indexTF).index);
     }
 
-    std::vector<int> cover;
-    float covered = intervalTC.start;
-
-    while (covered < intervalTC.end || intervalTC.end == intervalTC.start){
-        int indexTF = -1;
-        for (int i = intervals->size()-1; i >= 0; i--) {
-            if (covered > intervals->at(i).end) {
-                // Since sorted we know no more possible coverings
-                return std::vector<int>();
-            }
-            if (covered < intervals->at(i).start) {
-                continue; //Interval not covering beginning
-            }
-            indexTF = i;
-            covered = intervals->at(i).end;
-            break;
-        }
-        if (indexTF != -1) {
-            cover.push_back(intervals->at(indexTF).index);
-            intervals->erase(intervals->begin()+indexTF);
-        }else{
-            return std::vector<int>();
-        }
-        if (intervalTC.end == intervalTC.start) {
-            break;
-        }
+    /* Stop at the fist occurance of a valid result */
+    if (covered.end >= interval_goal.end){
+      break;
     }
-    if (covered < intervalTC.end){
-        return std::vector<int>();
-    }
+    indexTF++;
+  }
 
-    return cover;
+  // If we did not cover the entire goal interval return empty.
+  if (covered.end < interval_goal.end){
+    return std::vector<int>();
+  }
+
+  return cover;
 }
 
 int main(void) {
-    float start,end;
-    while(std::cin >> start >> end){
-        struct Interval intervalTC = {0, start, end};
+  float start,end;
+  while(std::cin >> start >> end){
+    struct interval interval_goal = {0, start, end};
 
-        std::vector<Interval> intervals;
-        int n;
-        std::cin >> n;
-        for (int j = 0; j < n; j++){
-            float start,end;
-            std::cin >> start >> end;
-            struct Interval interval = {j, start, end};
-            intervals.push_back(interval);
-        }
-
-        std::vector<int> output;
-        output = cover(intervalTC, &intervals);
-
-        if (output.empty()) {
-            std::cout << "impossible" << std::endl;
-        } else {
-            std::cout << output.size() << std::endl;
-            for (auto a : output){
-                std::cout << a << " ";
-            }
-            std::cout << std::endl;
-
-        }
+    std::vector<interval> intervals;
+    int n;
+    std::cin >> n;
+    for (int j = 0; j < n; j++){
+      float start,end;
+      std::cin >> start >> end;
+      struct interval inter = {j, start, end};
+      intervals.push_back(inter);
     }
+
+    std::vector<int> output;
+    output = cover(interval_goal, &intervals);
+
+    if (output.empty()) {
+      std::cout << "impossible" << std::endl;
+    } else {
+      std::cout << output.size() << std::endl;
+      for (auto a : output){
+        std::cout << a << " ";
+      }
+      std::cout << std::endl;
+
+    }
+  }
 }
 
