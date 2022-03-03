8868a1801cc15d1469363c61bb282217c1530bf1
Vollells <vicle728@student.liu.se>
2022-03-03 15:42:25 +0100

    fullTank using djikstras with table

---
+++ exercises/fullTank/fullTank.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <utility>
+
+template<typename T>
+struct Edge{
+  T endNode, gas, cost;
+  Edge(T nIn, T gIn,  long cIn) : endNode(nIn), gas(gIn), cost(cIn){}
+  bool operator > (const Edge &other) const {
+    return cost > other.cost;
+  }
+};
+
+template<typename T>
+struct Graph{
+  T N;
+  std::vector<std::vector<Edge<T>>> edges;
+  std::vector<std::vector<T>> gasCost;
+  std::vector<T> price;
+  Graph(T inN) : N(inN){
+    edges = std::vector<std::vector<Edge<T>>>(N);
+    gasCost = std::vector<std::vector<T>>(N, std::vector<T>(100, INT16_MAX));
+    price = std::vector<T>(N);
+  }
+};
+
+
+std::vector<Edge<int>> shortestPath (Graph<int> &graph, int startIndex, int cap){
+
+  std::vector<Edge<int>> result(graph.edges.size(), Edge<int>{-1, 0, INT32_MAX});
+  // Using edges as Nodes
+  std::priority_queue<Edge<int>, std::vector<Edge<int>>, std::greater<Edge<int>>> unsetNodes;
+
+  unsetNodes.emplace(startIndex, 0, 0);
+  result[startIndex] = Edge<int>(startIndex, 0, 0);
+
+  while(!unsetNodes.empty()){
+    Edge<int> curr = unsetNodes.top();
+    unsetNodes.pop();
+
+    //EndNode is actually current Node
+    if (graph.gasCost[curr.endNode][curr.gas] < curr.cost){continue;}
+
+    if(curr.gas < cap && curr.cost + graph.price[curr.endNode] < graph.gasCost[curr.endNode][curr.gas+1]){
+      graph.gasCost[curr.endNode][curr.gas+1] = curr.cost + graph.price[curr.endNode];
+      unsetNodes.emplace(curr.endNode, curr.gas+1, curr.cost + graph.price[curr.endNode]);
+    }
+
+    for(long i = 0; (unsigned long) i < graph.edges[curr.endNode].size(); i++){
+      long altIndex = graph.edges[curr.endNode][i].endNode;
+      long altCost = graph.edges[curr.endNode][i].cost;
+
+      if (curr.gas >= altCost){
+        int newGas = curr.gas - altCost;
+        if (curr.cost < graph.gasCost[altIndex][newGas]){
+          graph.gasCost[altIndex][newGas] = curr.cost;
+          unsetNodes.emplace(altIndex, newGas, curr.cost);
+          if(curr.cost < result[altIndex].cost){
+            result[altIndex] = Edge<int>(curr.endNode, newGas, curr.cost);
+          }
+        }
+      }
+    }
+  }
+
+  return result;
+}
+
+
+int main(void){
+
+  int M, N;
+  std::cin >> N >> M;
+
+  Graph<int> graph(N);
+
+  for (int i=0; i<N; i++){
+    std::cin >> graph.price[i];
+  }
+  for (int i=0; i<M; i++){
+    int U, V, D;
+    std::cin >> U >> V >> D;
+    graph.edges[U].emplace_back(V, 0, D);
+    graph.edges[V].emplace_back(U, 0, D);
+  }
+
+  int Q;
+  std::cin >> Q;
+  for(int i=0; i<Q; i++){
+    int C, S, E;
+    std::cin >> C >> S >> E;
+    std::vector<Edge<int>> result;
+    result = shortestPath(graph, S, C);
+
+    if (result[E].cost < INT16_MAX){
+      std::cout << result[E].cost << '\n';
+    } else {
+      std::cout << "impossible" << '\n';
+    }
+  }
+}
